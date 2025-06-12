ef537c3a11b7d5a3ca54edf40ccb9ec49d5b06fb
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-06-12 10:38:32 +0530

    Add initial setup for React To Do List application
    - Created manifest.json for PWA support
    - Added robots.txt for search engine directives
    - Implemented App.css for styling the application
    - Developed App.js as the main component, integrating ToDo component
    - Created App.test.js for basic testing of the App component
    - Established index.css for global styles
    - Set up index.js for rendering the application
    - Added info.js for educational comments on React components
    - Included logo.svg for application branding
    - Implemented reportWebVitals.js for performance tracking
    - Configured setupTests.js for testing environment
    - Developed toDo.js component for the To Do List functionality

---
+++ Frontend/React/first-react/src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import ToDo from './toDoList/toDo';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>Hello world</h1>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      <ToDo />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/React/first-react/src/toDoList/toDo.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class toDo extends Component {
+    render() {
+        return (
+            <>
+                <h1
+                style={{
+                    color: 'red',
+                    textAlign: "center"
+                }}>To Do List App</h1>
+            </>
+        )
+    }
+}
+
+export default toDo;
